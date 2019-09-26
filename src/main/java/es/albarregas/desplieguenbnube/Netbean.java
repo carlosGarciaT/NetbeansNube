a21e9e663e797155acc2d282fd465cd641ed6e10
LAPTOP-DN5BAGTQ\Carlos <carlosgt2603@gmail.com>
2019-09-26 16:44:18 +0200

    commint en local

---
+++ src/main/java/es/albarregas/desplieguenbnube/Netbean.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.albarregas.desplieguenbnube;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Netbean {
+    int i=5;
+    String saludo="Hola";
+    int p=i+3;
+    
+}
